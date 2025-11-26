91b95df8d3159542f1ab74a7ade6212f65d1bf87
Majid Uppal <mailtomajiduppal@gmail.com>
2025-11-25 18:58:14 -0600

    Adding the frontend folder and updated API

---
+++ src/api-service/api/utils.py
@@ -0,0 +1,14 @@
+"""
+Utility functions for mathematical operations
+Used by the Cheese App API Service
+"""
+
+
+def power(base,exponent):
+    """Calculate base raised to the power of exponent"""
+    return base**exponent
+
+
+
+def unused_function():
+    pass
